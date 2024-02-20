cc15255da7c10a1000b6da152120ba6d4984d49c
Agamagsm13 <slavagsm13@mail.ru>
2024-02-20 12:19:00 +0700

    ui for 5 letter words

--- app/src/main/java/com/example/worderworld/widget/CustomWordField.kt
+++ app/src/main/java/com/example/worderworld/widget/CustomWordField.kt
@@ -2,39 +2,43 @@ package com.example.worderworld.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.agamatech.worderworld.R
-import com.agamatech.worderworld.databinding.WidgetKeyViewBinding
+import com.agamatech.worderworld.databinding.WidgetWordViewBinding
+import com.example.worderworld.feature.game.LetterState
 
-class CustomKey @JvmOverloads constructor(
+class CustomWordField @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var binding: WidgetKeyViewBinding =
-        WidgetKeyViewBinding.inflate(LayoutInflater.from(context), this, true)
+    private var binding: WidgetWordViewBinding =
+        WidgetWordViewBinding.inflate(LayoutInflater.from(context), this, true)
 
-    private var keyText: String?
-    private var keyState: Int?
+    private var wordLength: Int?
 
     init {
-        val array = context.obtainStyledAttributes(attrs, R.styleable.KeyViewStyle, defStyleAttr, 0)
-        keyText = array.getText(R.styleable.KeyViewStyle_key_text).toString()
-        keyState = array.getInt(R.styleable.KeyViewStyle_key_state, 0)
+        val array = context.obtainStyledAttributes(attrs, R.styleable.WordViewStyle, defStyleAttr, 0)
+        wordLength = array.getInt(R.styleable.WordViewStyle_word_length, 0)
         setUI()
         array.recycle()
     }
 
     private fun setUI() {
-        binding.keyText.text = keyText
-        binding.keyBtn.setOnClickListener {
-            Toast.makeText(context, binding.keyText.text, Toast.LENGTH_LONG).show()
+        Log.e("idfjdfsdf", wordLength.toString())
+        for (i in 0..(wordLength?:0)) {
+            val letter = CustomLetter(context)
+            letter.changeLetterState(LetterState.INPUT)
+            letter.setText(" ")
+            binding.wordLayout.addView(CustomLetter(context = context))
         }
+        binding.wordLayout.invalidate()
     }
 
 
